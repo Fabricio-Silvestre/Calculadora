158d31717cef371213b7b07220d5444956b50ca2
Fabricio Silvestre <fabricio.amorimfsilvestre@gmail.com>
2024-11-23 12:26:35 -0300

    Atualizar o calculadora.py
    Atualizar o input que estava escrito errado

--- calculadora.py
+++ calculadora.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     while True:
         try:
             n1 = float(input('Digite o primeiro número: '))
-            n2 = float(input('Digite o primeiro número: '))
+            n2 = float(input('Digite o segundo número: '))
             break
         except ValueError:
             print(f'Erro: Valor digitado não é um número.')
